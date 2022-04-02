4ffd430b57880444c71986cbe16aebf663f697a3
Nick Forgione <nickforgione9@gmail.com>
2022-04-02 16:18:53 -0400

    Add project files.

---
+++ FunctionsP2.cpp
@@ -0,0 +1,186 @@
+#include <iostream>
+#include <time.h>
+#include <iomanip>
+#include "Sorts2.h"
+using namespace std;
+
+//
+//void selectionSort(int a[], int size) {
+//    int outer, inner, min;
+//    for (outer = 0; outer < size - 1; outer++) {
+//        min = outer;
+//        for (inner = outer + 1; inner < size; inner++) {
+//            if (a[inner] < a[min]) {
+//                min = inner;
+//            }
+//        }
+//        int temp = a[outer];
+//        a[outer] = a[min];
+//        a[min] = temp;
+//    }
+//}
+/////////////////////////////////////////////////
+//void insertionSort(int nums[], int numSize) {
+//    int i, j = 0;
+//    int temp = 0;
+//
+//    for (int i = 1; i < numSize; i++) {
+//        j = i;
+//        while (j > 0 && nums[j] < nums[j - 1]) {
+//            temp = nums[j];
+//            nums[j] = nums[j - 1];
+//            nums[j - 1] = temp;
+//            --j;
+//        }
+//    }
+//}
+/////////////////////////////////////////////////
+//void BubbleSort(int arr[], int size) {
+//
+//    for (int i = 0; i < size - 1; i++) {
+//        for (int j = 0; j < size - i - 1; j++) {
+//            if (arr[j] > arr[j + 1]) {
+//                int temp = arr[j];
+//                arr[j] = arr[j + 1];
+//                arr[j + 1] = temp;
+//            }
+//        }
+//    }
+//}
+//
+/////////////////////////////////////////////////
+//void Merge(int nums[], int i, int j, int k) {
+//    int mergedSize = k - i + 1;
+//    int mergePOS = 0;
+//    int leftPOS = 0;
+//    int rightPOS = 0;
+//    auto mergedNumbers = new int[mergedSize];
+//
+//
+//    leftPOS = i;
+//    rightPOS = j + 1;
+//
+//    while (leftPOS <= j && rightPOS <= k) {
+//        if (nums[leftPOS] <= nums[rightPOS]) {
+//            mergedNumbers[mergePOS] = nums[leftPOS];
+//            ++leftPOS;
+//        }
+//        else {
+//            mergedNumbers[mergePOS] = nums[rightPOS];
+//            ++rightPOS;
+//        }
+//        ++mergePOS;
+//    }
+//
+//    while (leftPOS <= j) {
+//        mergedNumbers[mergePOS] = nums[leftPOS];
+//        ++leftPOS;
+//        ++mergePOS;
+//    }
+//
+//    while (rightPOS <= k) {
+//        mergedNumbers[mergePOS] = nums[rightPOS];
+//        ++rightPOS;
+//        ++mergePOS;
+//    }
+//    for (mergePOS = 0; mergePOS < mergedSize; ++mergePOS) {
+//        nums[i + mergePOS] = mergedNumbers[mergePOS];
+//    }
+//}
+//
+//void mergeSort(int nums[], int i, int k) {
+//
+//    int j = 0;
+//
+//    if (i < k) {
+//        j = (i + k) / 2;
+//        mergeSort(nums, i, j);
+//        mergeSort(nums, j + 1, k);
+//        Merge(nums, i, j, k);
+//    }
+//}
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//void quickSort(int arr[], int left, int right) {
+//    int i = left, j = right;
+//    int tmp;
+//    int pivot = arr[(left + right) / 2];
+//
+//    /* partition */
+//    while (i <= j) {
+//        while (arr[i] < pivot)
+//            i++;
+//        while (arr[j] > pivot)
+//            j--;
+//        if (i <= j) {
+//            tmp = arr[i];
+//            arr[i] = arr[j];
+//            arr[j] = tmp;
+//            i++;
+//            j--;
+//        }
+//    };
+//
+//    if (left < j)
+//        quickSort(arr, left, j);
+//    if (i < right)
+//        quickSort(arr, i, right);
+//
+//}
+/////////////////////////////////////////////////
+//
+//
+//void heapify(int arr[], int x,  int i) {
+//
+//    int largest = i, l, r;
+//
+//    l = 2 * i + 1;
+//    r = 2 * i + 2;
+//
+//    if (l < x && arr[l] > arr[largest]) {
+//        largest = l;
+//    }
+//
+//    if (r < x && arr[r] > arr[largest]) {
+//        largest = r;
+//    }
+//
+//    if (largest != i) {
+//        swap(arr[i], arr[largest]);
+//        heapify(arr, x, largest);
+//    }
+//}
+//
+//void heapSort(int arr[], int x) {
+//    for (int i = x / 2 - 1; i >= 0; i--) {
+//        heapify(arr, x, i);
+//
+//    }
+//    for (int i = x - 1; i >= 0; i--) {
+//        swap(arr[0], arr[i]);
+//        heapify(arr, i, 0);
+//    }
+//}
+//
+////////////////////////////////////////////////////////
+//
+//void randArr(int arr[], int num) {
+//    for (int i = 0; i < num; i++) {
+//        int n = rand() % num;
+//        arr[i] = n;
+//    }
+//}
+//void print(int arr[], int sizeOf) {
+//    for (int i = 0; i < sizeOf - 1; i++) {
+//        cout << arr[i] << "\n";
+//        cout << arr[sizeOf];
+//    }
+//}
+//
+//
+//void reset(int arr[], int temp[], int num) {
+//    for (int i = 0; i < num; i++) {
+//        arr[i] = temp[i];
+//    }
+//
+//}
+//
---
+++ Sorts2.h
@@ -0,0 +1,16 @@
+//#pragma once
+//
+//#ifndef Sorts2_h 1
+//#define Sorts2_h
+//void randArr(int arr[], int num);
+//void selectionSort(int a[], int size);
+//void insertionSort(int nums[], int numSize);
+//void BubbleSort(int arr[], int size);
+//void Merge(int nums[], int i, int j, int k);
+//void mergeSort(int nums[], int i, int k);
+//void quickSort(int arr[], int left, int right);
+//void print(int arr[], int sizeOf);
+//void reset(int arr[], int temp[], int num);
+//void heapSort(int arr[], int x);
+//
+//#endif // !1
